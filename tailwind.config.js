1f83df41b2fb3f22096516cf6c9ceb80a91fd4db
Harshada <justinfo@zoho.com>
2026-03-10 16:41:48 +0530

    docs: added config file for tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./**/*.{html,js,php}", // This will scan all html, js, and php files in your project and subdirectories
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
