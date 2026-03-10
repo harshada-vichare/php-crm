c8dc6d7e7ee1d9a7c44f47572daafa5f456df079
Harshada <justinfo@zoho.com>
2026-03-10 20:40:16 +0530

    feat: added css to login page

--- login.php
+++ login.php
@@ -5,14 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login</title>
   </head>
-  <body>
-    <div>
-      <p>logo</p>
-      <form action="#" method="post">
-        <input type="text" placeholder="Username / Email" name="username" />
-        <input type="password" placeholder="Passowrd" name="password" />
-        <input type="submit" value="Login" />
+  <body
+    class="bg-gray-100 min-h-screen flex items-center justify-center font-mono"
+  >
+    <div class="bg-white w-1/4 p-14 rounded-xl shadow-lg">
+      <img
+        src="./public/company-logo.png"
+        alt="Company Logo"
+        class="h-24 mx-auto mb-10"
+      />
+      <form class="flex flex-col gap-6" action="#" method="post">
+        <input
+          class="bg-gray-200 text-center p-2 rounded-md border focus:border-blue-200 focus:outline-none"
+          type="text"
+          placeholder="Username / Email"
+          name="username"
+        />
+        <input
+          class="bg-gray-200 text-center p-2 rounded-md border focus:outline-none focus:border-blue-200"
+          type="password"
+          placeholder="Passowrd"
+          name="password"
+        />
+        <input
+          type="submit"
+          value="Login"
+          class="bg-blue-500 hover:bg-blue-600 px-4 text-sm mx-auto py-2 rounded-2xl uppercase"
+        />
       </form>
     </div>
+
+    <script src="https://cdn.tailwindcss.com"></script>
   </body>
 </html>
