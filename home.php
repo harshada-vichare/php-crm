0748e8b58761d1cc9ef12b82a13b7b56dd157aee
Harshada <justinfo@zoho.com>
2026-03-10 23:19:17 +0530

    refactor: added css to counts

--- home.php
+++ home.php
@@ -16,16 +16,34 @@
     <div
       class="main bg-white rounded w-auto shadow-md flex flex-grow text-center text-gray-800 justify-between gap-16 mx-auto my-10 px-10 py-16"
     >
-      <div class="bg-sky-200 rounded-lg w-40 p-12 h-40">
-        <p class="text-2xl italic">4</p>
+      <div
+        class="bg-sky-200 rounded-lg w-40 p-12 h-40 flex flex-col items-center justify-center"
+      >
+        <p
+          class="text-lg italic bg-red-800 w-10 h-10 p-2 rounded-full mb-3 text-white"
+        >
+          4
+        </p>
         <p class="font-bold">Customers</p>
       </div>
-      <div class="bg-lime-300 rounded w-40 p-12 h-40">
-        <p class="text-2xl italic">15</p>
+      <div
+        class="bg-lime-300 rounded w-40 p-12 h-40 flex flex-col items-center justify-center"
+      >
+        <p
+          class="text-lg italic bg-red-800 w-10 h-10 p-2 rounded-full mb-3 text-white"
+        >
+          15
+        </p>
         <p class="font-bold">Products</p>
       </div>
-      <div class="bg-purple-300 rounded w-40 p-12 h-40">
-        <p class="text-2xl italic">12</p>
+      <div
+        class="bg-purple-300 rounded w-40 p-12 h-40 flex flex-col items-center justify-center"
+      >
+        <p
+          class="text-lg italic bg-red-800 w-10 h-10 p-2 rounded-full mb-3 text-white"
+        >
+          12
+        </p>
         <p class="font-bold">Leads</p>
       </div>
     </div>
