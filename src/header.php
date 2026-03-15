2b24da887704de2de86ed2939d33b0cf054b2beb
Harshada <justinfo@zoho.com>
2026-03-15 19:53:03 +0530

    refractor: adjusted title

--- src/header.php
+++ src/header.php
@@ -1,6 +1,7 @@
+<?php require_once('./check-url.php'); ?>
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Home</title>
+  <title><?php print_r(ucfirst(explode('.', $last_part)[0])); ?></title>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
