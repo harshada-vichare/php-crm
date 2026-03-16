54baacb198e49ceccf1db15ab75d39076c93030c
Harshada <justinfo@zoho.com>
2026-03-16 23:09:29 +0530

    refactor: added link to boxes

--- home.php
+++ home.php
@@ -7,36 +7,42 @@
     <div
       class="main bg-white rounded w-auto shadow-md flex flex-grow text-center text-gray-800 justify-between gap-16 mx-auto my-10 px-10 py-16"
     >
-      <div
-        class="bg-sky-200 rounded-lg w-40 p-12 h-40 flex flex-col items-center justify-center"
-      >
-        <p
-          class="text-lg italic bg-red-800 w-10 h-10 p-2 rounded-full mb-3 text-white"
+      <a href="./customers.php">
+        <div
+          class="bg-sky-200 rounded-lg w-40 p-12 h-40 flex flex-col items-center justify-center"
         >
-          4
-        </p>
-        <p class="font-bold">Customers</p>
-      </div>
-      <div
-        class="bg-lime-300 rounded w-40 p-12 h-40 flex flex-col items-center justify-center"
-      >
-        <p
-          class="text-lg italic bg-red-800 w-10 h-10 p-2 rounded-full mb-3 text-white"
+          <p
+            class="text-lg italic bg-red-800 w-10 h-10 p-2 rounded-full mb-3 text-white"
+          >
+            4
+          </p>
+          <p class="font-bold">Customers</p>
+        </div>
+      </a>
+      <a href="./products.php">
+        <div
+          class="bg-lime-300 rounded w-40 p-12 h-40 flex flex-col items-center justify-center"
         >
-          15
-        </p>
-        <p class="font-bold">Products</p>
-      </div>
-      <div
-        class="bg-purple-300 rounded w-40 p-12 h-40 flex flex-col items-center justify-center"
-      >
-        <p
-          class="text-lg italic bg-red-800 w-10 h-10 p-2 rounded-full mb-3 text-white"
+          <p
+            class="text-lg italic bg-red-800 w-10 h-10 p-2 rounded-full mb-3 text-white"
+          >
+            15
+          </p>
+          <p class="font-bold">Products</p>
+        </div>
+      </a>
+      <a href="./leads.php">
+        <div
+          class="bg-purple-300 rounded w-40 p-12 h-40 flex flex-col items-center justify-center"
         >
-          12
-        </p>
-        <p class="font-bold">Leads</p>
-      </div>
+          <p
+            class="text-lg italic bg-red-800 w-10 h-10 p-2 rounded-full mb-3 text-white"
+          >
+            12
+          </p>
+          <p class="font-bold">Leads</p>
+        </div>
+      </a>
     </div>
 
     <?php include_once('./src/footer.php'); ?>
