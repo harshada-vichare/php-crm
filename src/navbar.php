45fda7709ea66136553fab170f1936afd638305e
Harshada <justinfo@zoho.com>
2026-03-17 21:18:27 +0530

    refactor: pages are accessible for logged in users only

--- src/navbar.php
+++ src/navbar.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if(isset($_SESSION['username']) && isset($_SESSION['role']) && $_SESSION['role'] === 'admin'){
+  $username = $_SESSION['username'];
+}else{header('Location: ./login.php');exit;}
+?>
+
 <?php require_once('./check-url.php'); ?>
 <div class="navbar border-b-2 flex justify-between p-4 items-center">
   <img src="./public/company-logo.png" alt="Company Logo" class="h-16" />
@@ -6,9 +13,9 @@
     if(!in_array($last_part, $nameArr)){ ?>
     <a href="./home.php" class="mr-10">Home</a>
     <?php } ?>
-    <span><em>Hello X</em></span>
+    <span><em>Hello <?php echo $username; ?></em></span>
   </div>
   <button class="bg-blue-500 hover:bg-blue-600 rounded-2xl py-2 px-3">
-    Logout
+    <a href="./logout.php">Logout</a>
   </button>
 </div>
