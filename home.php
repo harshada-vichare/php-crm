c822fea5f43c67fd5d858ec34ed3068f7daafe7e
Harshada <justinfo@zoho.com>
2026-03-17 23:40:16 +0530

    refactor: count data come from database

--- home.php
+++ home.php
@@ -3,11 +3,23 @@
   <?php require_once('./src/header.php'); ?>
 
   <body class="bg-gray-100 min-h-screen font-mono flex flex-col">
-    <?php require_once('./src/navbar.php'); ?>
+    <?php require_once('./src/navbar.php'); require_once('./src/db.php'); ?>
 
     <div
       class="main bg-white rounded w-auto shadow-md flex flex-grow text-center text-gray-800 justify-between gap-16 mx-auto my-10 px-10 py-16"
     >
+      <?php 
+      $products_sql = "SELECT `id` FROM `products` WHERE `is_deleted` = 'N'"; 
+      $products_result = $conn->query($products_sql);
+
+      
+      $customers_sql = "SELECT `id` FROM `customers` WHERE `is_customer` = 'Y'"; 
+      $customers_result = $conn->query($customers_sql);
+
+      // not selected which count to show - won | lost | all
+      $leads_sql = "SELECT `id` FROM `leads`"; 
+      $leads_result = $conn->query($leads_sql);
+      ?>
       <a href="./customers.php">
         <div
           class="bg-sky-200 rounded-lg w-40 p-12 h-40 flex flex-col items-center justify-center"
@@ -15,7 +27,7 @@
           <p
             class="text-lg italic bg-red-800 w-10 h-10 p-2 rounded-full mb-3 text-white"
           >
-            4
+          <?php echo $customers_result->num_rows; ?>
           </p>
           <p class="font-bold">Customers</p>
         </div>
@@ -27,7 +39,7 @@
           <p
             class="text-lg italic bg-red-800 w-10 h-10 p-2 rounded-full mb-3 text-white"
           >
-            15
+          <?php echo $products_result->num_rows; ?>
           </p>
           <p class="font-bold">Products</p>
         </div>
@@ -39,7 +51,7 @@
           <p
             class="text-lg italic bg-red-800 w-10 h-10 p-2 rounded-full mb-3 text-white"
           >
-            12
+          <?php echo $leads_result->num_rows; ?>
           </p>
           <p class="font-bold">Leads</p>
         </div>
